d121547ea57e3d43b031995f0ab4014a9ea09461
pereira-lucasap <pereira.lucasap@gmail.com>
2023-04-01 16:08:06 -0300

    modified:   controllers/UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,6 @@
 class UserController {
 
-    constructor(formId, tableId){
+    constructor(formId, tableId) {
 
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
@@ -9,21 +9,27 @@ class UserController {
 
     }
 
-    onSubmit(){
+    onSubmit() {
 
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]");
+            btn.disabled = true;
+
             let values = this.getValues();
 
             this.getPhoto().then(
-                
-                function(content){
+                (content) => {
                     values.photo = content;
                     this.addLine(values);
-                }, 
-                function(e){
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+                },
+                function (e) {
                     console.error("ERRO DA PROMESSA", e);
                 }
             );
@@ -31,93 +37,100 @@ class UserController {
         });
     }//onSubmit
 
-    getPhoto(){
+    getPhoto() {
 
-        return new Promise(function(resolve, reject){
+        return new Promise((resolve, reject) => {
 
             let fileReader = new FileReader();
 
             let elements = [...this.formEl.elements].filter(item => {
 
-                if (item.name === 'photo'){
+                if (item.name === 'photo') {
                     return item;
                 }
 
             });
 
-            console.log( elements[0].files[0]);
             let file = elements[0].files[0];
 
-            fileReader.onload = ()=>{
+            fileReader.onload = () => {
 
                 resolve(fileReader.result);
 
             };
-            fileReader.onerror = (e)=>{
+            fileReader.onerror = (e) => {
+
                 reject(e);
+
             };
 
-            fileReader.readAsDataURL(file);
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         });
 
-    }//getPhoto
+    }//getPhoto Promise
 
-    getValues(){
+    getValues() {
 
         let user = {};
 
-        [...this.formEl.elements].forEach(function(field, index){
-            if(field.name == "gender"){
-        
-                if(field.checked){
-        
+        [...this.formEl.elements].forEach(function (field, index) {
+            if (field.name == "gender") {
+
+                if (field.checked) {
+
                     user[field.name] = field.value;
-        
+
                 }
+
+
+            } else if (field.name == "admin") {
                 
-        
-            }else {
-        
+                user[field.name] = field.checked;
+
+            } else {
+
                 user[field.name] = field.value;
-        
+
             }
-            
+
         });
         return new User(
-            user.name, 
-            user.gender, 
-            user.birth, 
-            user.coutry, 
-            user.email, 
-            user.password, 
-            user.photo, 
+            user.name,
+            user.gender,
+            user.birth,
+            user.coutry,
+            user.email,
+            user.password,
+            user.photo,
             user.admin
-            ); 
+        );
 
-        
+
     }//getValues
 
-    addLine(dataUser){
+    addLine(dataUser) {
 
-        console.log(dataUser)
-    
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr')
+
+        tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin ? 'Sim' : 'Não')}</td>
             <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-      </tr>
       `;
-    
-      document.getElementById("table-users").appendChild(tr);
-    
+
+        this.tableEl.appendChild(tr);
+
     }//addLine
 
 }//UserController
